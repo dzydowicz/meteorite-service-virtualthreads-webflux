843180d09146efe978fe19ea7b8ff8ce1f139068
damianzydowicz <damian.zydowicz@gmail.com>
2025-01-14 19:42:33 +0100

    CalculationsService: Changed types to float

--- virtual-threads-app/src/main/java/com/dzydowicz/virtualthreads/application/dto/calculations/CalculateKineticEnergyResponse.java
+++ virtual-threads-app/src/main/java/com/dzydowicz/virtualthreads/application/dto/calculations/CalculateKineticEnergyResponse.java
@@ -8,5 +8,5 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CalculateKineticEnergyResponse {
-    private Double kineticEnergy;
+    private Float kineticEnergy;
 }
